8e6008983e821076288fd0bb24760278e933baf6
Vit Mur <myp1blka@gmail.com>
2022-06-08 17:52:57 +0300

    format and clear code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+
 const elKnopka_start = document.querySelector('input[name="knopka_hello"]');
 const elFirstName = document.querySelector('input[name="first-name"]');
 const elSecondName = document.querySelector('input[name="last-name"]');
 const elStatus = document.querySelector('.status')
-
 
 const run = (ev) => {
     elStatus.innerHTML = '> ';
@@ -10,21 +10,9 @@ const run = (ev) => {
     const valueSecondName = elSecondName.value;
     let result = `Привіт,  ${valueFirstName} ${ valueSecondName }`;
 
-elStatus.insertAdjacentHTML('beforeend', result);
-
-
-
-
-
-
     // почему не работает такая конструкция
     //elStatus.insertAdjacentHTML('beforeend', 'Привіт, ' , elFirstName.value, elSecondName.value);
+    elStatus.insertAdjacentHTML('beforeend', result);
 }
-
-
-
-
-
-
 
 elKnopka_start.addEventListener('click', run);
